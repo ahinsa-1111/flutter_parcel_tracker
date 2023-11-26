756a13abbdcc89f36cc57ace0093c8536414c98c
ahinsa-1111 <148263893+ahinsa-1111@users.noreply.github.com>
2023-11-26 20:38:21 +0530

    commit_04

--- lib/main.dart
+++ lib/main.dart
@@ -19,32 +19,61 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget content() {
-    return Container(
-      height: 300,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(30),
-          bottomRight: Radius.circular(30),
-        ),
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Column(children: [
-          Image.network(
-            "https://st3.depositphotos.com/7177640/15256/v/1600/depositphotos_152568528-stock-illustration-vector-cartoon-delivery-man-with.jpg",
-            height: 200,
-            width: 200,
-          ),
-          Text(
-            "Parcel Tracker",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+    return Column(
+      children: [
+        Container(
+          height: 300,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
             ),
-          )
-        ]),
-      ),
+          ),
+          child: Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Column(children: [
+                Image.network(
+                  "https://st3.depositphotos.com/7177640/15256/v/1600/depositphotos_152568528-stock-illustration-vector-cartoon-delivery-man-with.jpg",
+                  height: 200,
+                  width: 200,
+                ),
+                Text(
+                  "Parcel Tracker",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ]),
+            ),
+          ),
+        ),
+        body()
+      ],
+    );
+  }
+
+  Widget body() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 30,
+        ),
+        Text(
+          "Tracking Number :",
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+      ],
     );
   }
 }
