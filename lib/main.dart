8c52d7062c237c0c9a3429ce371da18731065ad7
ahinsa-1111 <148263893+ahinsa-1111@users.noreply.github.com>
2023-11-26 20:27:01 +0530

    commit_03

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,23 @@ class _HomePageState extends State<HomePage> {
           bottomRight: Radius.circular(30),
         ),
       ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Column(children: [
+          Image.network(
+            "https://st3.depositphotos.com/7177640/15256/v/1600/depositphotos_152568528-stock-illustration-vector-cartoon-delivery-man-with.jpg",
+            height: 200,
+            width: 200,
+          ),
+          Text(
+            "Parcel Tracker",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
